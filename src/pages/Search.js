acfc163ba3c23478d8e4ea24b5f41c53e7a3ef43
Thawane <thawaneapoliveira@gmail.com>
2022-10-03 20:26:50 -0300

    Adiciona requisito 6

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,21 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
-// import { BrowserRouter, Route } from 'react-router-dom';
+import searchAlbumsAPI from '../services/searchAlbumsAPI';
+import NowLoading from './NowLoading';
 
 class Search extends React.Component {
   constructor() {
     super();
 
     this.state = {
-      searchInput: '',
+      displayResult: false,
       isButtonDisabled: true,
+      isLoading: false,
+      searchValue: '',
+      research: '',
+      searchResult: '',
     };
   }
 
   enableSearchButton = () => {
-    const { searchInput } = this.state;
+    const { searchValue } = this.state;
     const minLength = 2;
-    if (searchInput.length >= minLength) {
+    if (searchValue.length >= minLength) {
       this.setState({ isButtonDisabled: false });
     } else {
       this.setState({ isButtonDisabled: true });
@@ -23,37 +29,84 @@ class Search extends React.Component {
   };
 
   onInputChange = (event) => {
-    const { name, value } = event.target;
-    this.setState({ [name]: value }, this.enableSearchButton);
+    const { value } = event.target;
+    this.setState({ searchValue: value }, this.enableSearchButton);
+  };
+
+  onButtonClick = async () => {
+    const { searchValue } = this.state;
+    this.setState({ isLoading: true, research: searchValue });
+    const apiResponse = await searchAlbumsAPI(searchValue);
+    // console.log(typeof searchInput);
+    this.setState({
+      isLoading: false,
+      searchValue: '',
+      searchResult: apiResponse,
+      displayResult: true,
+    }, this.enableSearchButton);
   };
 
   render() {
-    const { searchInput, isButtonDisabled } = this.state;
+    const {
+      searchValue, research, isButtonDisabled, isLoading, searchResult, displayResult,
+    } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
 
-        <fieldset>
+        {isLoading ? <NowLoading />
 
-          <label htmlFor="searchInput">
-            <input
-              type="text"
-              data-testid="search-artist-input"
-              name="searchInput"
-              id="searchInput"
-              value={ searchInput }
-              onChange={ this.onInputChange }
-            />
-          </label>
-          <button
-            type="button"
-            data-testid="search-artist-button"
-            disabled={ isButtonDisabled }
-          >
-            Entrar
-          </button>
+          : (
+            <fieldset>
 
-        </fieldset>
+              <label htmlFor="searchValue">
+                <input
+                  type="text"
+                  data-testid="search-artist-input"
+                  name="searchValue"
+                  id="searchValue"
+                  value={ searchValue }
+                  onChange={ this.onInputChange }
+                />
+              </label>
+              <button
+                type="button"
+                data-testid="search-artist-button"
+                disabled={ isButtonDisabled }
+                onClick={ this.onButtonClick }
+              >
+                Pesquisar
+              </button>
+
+            </fieldset>
+          )}
+        { displayResult && (
+          <>
+            <span>
+              {`Resultado de álbuns de:
+              ${research}`}
+            </span>
+            {
+              searchResult.length === 0
+                ? <span> Nenhum álbum foi encontrado</span>
+                : searchResult.map((item, id) => (
+                  <div key={ id }>
+                    <p>{item.artistName}</p>
+                    <p>{item.collectionName}</p>
+                    <img src={ item.artworkUrl100 } alt={ item.artistName } />
+
+                    <Link
+                      to={ `/album/${item.collectionId}` }
+                      data-testid={ `link-to-album-${item.collectionId}` }
+                    >
+                      <p>Ir para a página do álbum</p>
+                    </Link>
+
+                  </div>
+                ))
+            }
+          </>
+        )}
       </div>
     );
   }
