d8ae26abb25b2852d829f26f950c0226f49d6bda
Thawane <thawaneapoliveira@gmail.com>
2022-10-05 11:25:13 -0300

    Adiciona requisito 13

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import { getUser } from '../services/userAPI';
+import NowLoading from './NowLoading';
 // import { BrowserRouter, Route } from 'react-router-dom';
 
 class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loginName: '',
+      isLoading: true,
+    };
+  }
+
+  async componentDidMount() {
+    const loginName = await getUser();
+    this.setState({ isLoading: false, loginName });
+  }
+
   render() {
+    const { isLoading, loginName } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        { isLoading ? <NowLoading />
+          : (
+            <>
+              <Link to="/profile/edit">
+                Editar perfil
 
-        <p>page-profile</p>
+              </Link>
+              <img
+                src={ loginName.image }
+                alt={ loginName.name }
+                data-testid="profile-image"
+              />
+              <p>{loginName.name}</p>
+              <p>{loginName.email}</p>
+              <p>{loginName.description}</p>
+            </>
+          )}
       </div>
     );
   }
